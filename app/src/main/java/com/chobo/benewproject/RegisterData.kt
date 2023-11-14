ac89c7e1ca6143cf4c09c338688997407389b30f
Danny0809 <mckdb1566@naver.com>
2023-11-14 13:30:09 +0900

    registerv2

--- app/src/main/java/com/chobo/benewproject/RegisterData.kt
+++ app/src/main/java/com/chobo/benewproject/RegisterData.kt
@@ -10,9 +10,25 @@ data class RegisterData (
     private var password: String,
 
     @SerializedName("name")
-    private var name: String
+    private var name: String,
+
+    @SerializedName("gender")
+    private var gender: String,
+
+    @SerializedName("birthdate")
+    private var birthdate: String,
+
+    @SerializedName("email")
+    private var email: String,
+
+    @SerializedName("major")
+    private var major: String,
+
+    @SerializedName("phoneNumber")
+    private var phoneNumber: String
 ){
     override fun toString() : String{
-        return "account=$account, password=$password, name=$name"
+        return "account=$account, password=$password, name=$name, gender=$gender," +
+                "birthdate=$birthdate, email=$email, major=$major, phoneNumber=$phoneNumber"
     }
 }
